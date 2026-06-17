2f87b839ff97470366287f298b8ed060103f2818
ecstacy0ne <lev.loskutov@inbox.ru>
2026-06-17 22:05:00 +0300

    fix для хостинга

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// чтобы каждый раз не переключать createwebhistory (для просмотра на локалхост) на createwebhashhistory (для использования на gh-pages)
 export default defineConfig(({ mode }) => {
-  const isProd = mode === 'production' //даёт true при выполнении npm run build
+  const isProd = mode === 'production'
+
+  // Если сборка идет на Vercel — ставим '/', если в продакшн для GH Pages — '/educational/'
+  const basePath = process.env.VERCEL ? '/' : (isProd ? '/educational/' : '/')
 
   return {
     plugins: [vue()],
-    base: isProd ? "/educational/" : "/",
+    base: basePath,
   }
 })
